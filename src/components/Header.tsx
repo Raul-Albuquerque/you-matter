dd12e8b6e7e28d54280f0e6186f3af06b2ae65c6
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-12-11 15:07:33 -0300

    feat: new quotes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
+import Link from 'next/link'
 import Logo from '../../public/heart.svg'
 import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <header className="w-full h-28 bg-neutral-50 ">
-      <div className="w-full max-w-[1024px] mx-auto flex items-center py-5">
+    <header className="w-full h-28 bg-neutral-50">
+      <Link
+        href={'/'}
+        className="w-[80%] max-w-[1024px] mx-auto flex items-center py-5"
+      >
         <Image className="h-20" src={Logo} alt="logo" />
         <h1 className="font-shizuru text-4xl ml-4 text-red-600">YouMATTER</h1>
-      </div>
+      </Link>
     </header>
   )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,17 @@ const MotivationalQuotes = [
   'Somos capazes de fazer muito mais do que imaginamos!',
   'Abra os olhos e aventure-se nessa jornada chamada vida!',
   'Siga sempre confiante do seu potencial!',
+  'Quando seu coração está pleno de gratidão, qualquer porta aparentemente fechada pode ser uma abertura para uma bênção maior.',
+  'A gratidão é uma grande aliada do sucesso!',
+  'A ideia é nunca perder a motivação para subir cada degrau rumo ao sucesso, por mais difícil que seja. E também não esquecer de agradecer pelas lições aprendidas em cada etapa da jornada.',
+  'É importante agradecer pelo hoje sem nunca desistir do amanhã!',
+  'A vida reserva surpresas maravilhosas para todos aqueles que cultivam a gratidão!',
+  'Lembre-se do seu passado com carinho. Afinal, é por causa dele que você está aqui hoje!',
   'Hoje é um novo dia. Não fique pensando no ontem.',
+  'Agradeça todos os dias pela sua vida, por tudo o que você conquistou até aqui. E tenha fé de que o futuro lhe reserva realizações ainda maiores!',
+  'Se expressarmos gratidão pelo que temos, teremos mais por que expressar gratidão.',
+  'Desejar algo maior pra sua vida não significa que você está insatisfeito com o que tem.',
+  'Acorde todas as manhãs com um sorriso. Esta é mais uma oportunidade que você tem para ser feliz. Seja seu próprio motor de ignição. O dia de hoje jamais voltará .... Não o desperdice!!!',
   'Quem cultiva gratidão é capaz de realizar sonhos que parecem inalcançáveis!',
   'Só tenho a agradecer a cada um dos desafios que me trouxeram até aqui, que me fizeram crescer e aprender tantas coisas!',
   'Não há a necessidade de caminhar rápido. Apenas siga caminhando.',
@@ -26,6 +36,16 @@ const DemotivationalQuotes = [
   'Nunca desista, faça até dar errado.',
   'Tudo aquilo que você passou até agora está te preparando para algo pior.',
   'Um dia você perde. No outro você não ganha.',
+  'O caminho é longo, mas a derrota é certa.',
+  'É só uma fase ruim, logo ela vai piorar.',
+  'Pare de desejar e comece a desistir.',
+  'Nunca foi azar, sempre foi incompetência.',
+  'Você é mais fraco do que pensa.',
+  'Você não é incrível.',
+  'Quando sua mente pensar em desistir, desista.',
+  'Para ficar ruim, você terá que melhorar muito.',
+  'Nunca subestime a sua incapacidade.',
+  'Durma 12 horas por dia, assim as chances de você fazer merda caem pela metade.',
 ]
 
 export function Main() {
@@ -35,7 +55,7 @@ export function Main() {
 
   const getQuote = () => {
     const getRandomNumber = () => {
-      const randomNumber = Math.floor(Math.random() * 10)
+      const randomNumber = Math.floor(Math.random() * 20)
       return randomNumber
     }
     setIsActive(true)
@@ -49,33 +69,33 @@ export function Main() {
 
   return (
     <>
-      <section className="w-full max-w-[1024px] mx-auto rounded-lg mt-14 p-10">
+      <section className="w-[80%] max-w-[1024px] mx-auto rounded-lg mt-14 p-10w">
         <h2 className="text-center font-maven text-red-600 font-bold text-xl">
           ESCOLHA UMA DAS OPÇÕES ABAIXO E SINTA-SE MELHOR:
         </h2>
-        <div className="mx-auto w-full flex items-center justify-around mt-10">
+        <div className="flex flex-col mx-auto w-full items-center justify-around mt-10 lg:flex-row ">
           <button
             onClick={() => {
               getQuote()
               setIsMotivational(true)
             }}
-            className="bg-red-500 text-neutral-50 text-center font-bold px-6 py-4 rounded-lg"
+            className="block mb-4 w-full bg-red-500 text-neutral-50 text-center font-bold sm:px-6 py-4 rounded-lg lg:mb-0"
           >
-            MENSAGEM MOTIVACIONAL
+            QUERO ME SENTIR BEM
           </button>
           <button
             onClick={() => {
               getQuote()
               setIsMotivational(false)
             }}
-            className="bg-red-500 text-neutral-50 text-center font-bold px-6 py-4 rounded-lg"
+            className="block w-full bg-red-500 text-neutral-50 text-center font-bold sm:px-6 py-4 rounded-lg lg:ms-2"
           >
-            MENSAGEM DESMOTIVACIONAL
+            QUERO ME SENTIR MELHOR AINDA
           </button>
         </div>
       </section>
       {isActive && (
-        <section className="w-full max-w-[1024px] mx-auto rounded-lg mt-3 p-5">
+        <section className="w-[80%] max-w-[1024px] mx-auto rounded-lg mt-3 p-5 md:w-[80%]">
           <div className="bg-[url('/notebook.svg')] bg-no-repeat mx-auto max-w-[588px] max-h-[260px] p-10">
             {isMotivational ? (
               <p className="text-center font-whisper text-3xl">
